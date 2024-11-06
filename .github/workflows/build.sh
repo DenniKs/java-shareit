5694889f10a622654eccf58c6937e241c1e22fdd
DenniKs <dennikz@yandex.ru>
2024-11-07 02:25:03 +0300

    fix: корректировка версии java проекта спринта 16

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,2 +1,8 @@
-mvn install -P check,coverage
-docker-compose build
+cp -rf ./tests/checkstyle.xml ./checkstyle.xml
+cp -rf ./tests/suppressions.xml ./suppressions.xml
+cp -rf ./tests/gateway/Dockerfile ./gateway/Dockerfile
+cp -rf ./tests/server/Dockerfile ./server/Dockerfile
+cp -rf ./tests/docker-compose.yml ./docker-compose.yml
+mvn enforcer:enforce -Denforcer.rules=requireProfileIdsExist -P check --no-transfer-progress &&
+mvn verify -P check,coverage --no-transfer-progress &&
+docker compose -f docker-compose.yml build
