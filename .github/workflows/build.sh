4298c2daff7131f1058e84dfca8154a2d06c310f
DenniKs <dennikz@yandex.ru>
2024-11-07 03:08:54 +0300

    fix: корректировка версии java проекта спринта 16

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,8 +1,2 @@
-cp -rf ./tests/checkstyle.xml ./checkstyle.xml
-cp -rf ./tests/suppressions.xml ./suppressions.xml
-cp -rf ./tests/gateway/Dockerfile ./gateway/Dockerfile
-cp -rf ./tests/server/Dockerfile ./server/Dockerfile
-cp -rf ./tests/docker-compose.yml ./docker-compose.yml
-mvn enforcer:enforce -Denforcer.rules=requireProfileIdsExist -P check --no-transfer-progress &&
-mvn verify -P check,coverage --no-transfer-progress &&
-docker compose -f docker-compose.yml build
+mvn install -P check,coverage
+docker-compose build
--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,10 +1,5 @@
+docker-compose up --detach
 chmod a+x ./tests/.github/workflows/wait-for-it.sh
-docker compose -f docker-compose.yml up --detach  &&
-echo "Docker is up" &&
-./tests/.github/workflows/wait-for-it.sh -t 60 localhost:9090 &&
-echo "Server is up" &&
-./tests/.github/workflows/wait-for-it.sh -t 60 localhost:8080 &&
-echo "Gateway is up"
-result=$?
-docker compose -f docker-compose.yml logs
-exit $result
+./tests/.github/workflows/wait-for-it.sh -t 60 localhost:8080
+./tests/.github/workflows/wait-for-it.sh -t 60 localhost:9090
+docker-compose logs
